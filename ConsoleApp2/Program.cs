a1641900beb0f8a10074e2046125a784e49680a0
liaobin0204 <liaobin0204@hotmail.com>
2018-02-22 00:10:17 +0800

    第二次修改

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -10,7 +10,8 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("1");
+            //Console.WriteLine("1");
+            Console.WriteLine("2");
             
         }
     }
